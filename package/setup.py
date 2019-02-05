81a135fa0ba07fce2b2f4e353920bc2b88256718
RolefH <rolef.heinrich@de.ibm.com>
2019-02-05 09:09:13 +0100

    Development Status: 5 - Production/Stable

--- package/setup.py
+++ package/setup.py
@@ -13,7 +13,7 @@ setup(
   url = 'https://github.com/IBMStreams/pypi.streamsx.kafka',
   keywords = ['streams', 'ibmstreams', 'streaming', 'analytics', 'streaming-analytics', 'messaging', 'kafka'],
   classifiers = [
-    'Development Status :: 4 - Beta',
+    'Development Status :: 5 - Production/Stable',
     'License :: OSI Approved :: Apache Software License',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
