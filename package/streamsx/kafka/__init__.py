c580aba2cda4d69367b55f7be37914eab280abd8
rolefhei <rolefhei@de.ibm.com>
2019-05-24 09:05:32 +0200

    publish corrected

--- package/streamsx/kafka/__init__.py
+++ package/streamsx/kafka/__init__.py
@@ -99,7 +99,7 @@ a topic and the same application consuming the same topic::
 """
 
 
-__version__='1.2.0'
+__version__='1.2.1'
 
 __all__ = ['subscribe', 'publish', 'configure_connection']
 from streamsx.kafka._kafka import subscribe, publish, configure_connection
